01f42ed71e150b785db2570f34bccff74a915a0e
catsareepi <113269139+catsareepi@users.noreply.github.com>
2022-09-26 18:53:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mariodie = loadSound('mariodie.wav');
+	jump = loadSound('jump.wav');
+	kick = loadSound('kick.wav');
+	mariocoin = loadSound('coin.wav');
+	gameover = loadSound('gameover.wav');
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+	posenet = ml5.poseNet(video, modelLoaded);
+	posenet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+	console.log('model loaded');
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
